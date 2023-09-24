460121eb9381ec99a2a2ee44e0357531bce0b695
Kendall-22 <107010388+Kendall-22@users.noreply.github.com>
2023-09-24 12:06:53 -0600

    Commit 0 de clase Sem3

---
+++ WebMN/WebMN/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMN.Entities;
+using WebMN.Models;
+
+namespace WebMN.Controllers
+{
+    public class LoginController : Controller
+    {
+        UsuarioModel usuarioModel = new UsuarioModel();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(UsuarioEnt entidad)
+        {
+            usuarioModel.IniciarSesion(entidad);
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RegistrarCuenta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegistrarCuenta(UsuarioEnt entidad)
+        {
+            usuarioModel.RegistrarCuenta(entidad);
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult RecuperarCuenta()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarCuenta(UsuarioEnt entidad)
+        {
+            usuarioModel.RecuperarCuenta(entidad);
+            return View();
+        }
+
+    }
+}
---
+++ WebMN/WebMN/Models/UsuarioModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebMN.Entities;
+
+namespace WebMN.Models
+{
+    public class UsuarioModel
+    {
+
+        public void IniciarSesion(UsuarioEnt entidad)
+        { 
+            //LLAMAR AL WEB API PARA VALIDAR EL USUARIO
+        }
+
+        public void RegistrarCuenta(UsuarioEnt entidad)
+        {
+            //LLAMAR AL WEB API PARA REGISTRAR EL USUARIO
+        }
+
+        public void RecuperarCuenta(UsuarioEnt entidad)
+        {
+            //LLAMAR AL WEB PARA VALIDAR EL USUARIO Y ENVIARLE UN CORREO
+        }
+
+    }
+}
